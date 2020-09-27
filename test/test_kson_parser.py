854306d5532118e1bd458401b0a59ca8c3c1821d
Tom Ritchford <tom@swirly.com>
2020-09-27 11:02:46 +0200

    Add a round-trip test

--- test/test_kson_parser.py
+++ test/test_kson_parser.py
@@ -1,5 +1,4 @@
 from kson.read import decoder
-import json
 import lark
 import unittest
 
@@ -7,20 +6,6 @@ parse = decoder.DECODER
 
 
 class KsonParserTest(unittest.TestCase):
-    def test_original(self):
-        test_json = """
-            {
-                "empty_object" : {},
-                "empty_array"  : [],
-                "booleans"     : { "YES" : true, "NO" : false },
-                "numbers"      : [ 0, 1, -2, 3.3, 4.4e5, 6.6e-7 ],
-                "strings"      : [ "This", [ "And" , "That", "And a \\"b" ] ],
-                "nothing"      : null
-            }
-        """
-        j = parse(test_json)
-        assert j == json.loads(test_json)
-
     def test_comma_list(self):
         for e in '[1,]', '[,1]', '[,1,]':
             assert parse(e) == [1]
---
+++ test/test_round_trip.py
@@ -0,0 +1,44 @@
+from kson.read import decoder
+from kson.write import writer
+import json
+import unittest
+
+
+class RoundTripTest(unittest.TestCase):
+    def test_json(self):
+        test_json = """
+            {
+                "empty_object" : {},
+                "empty_array"  : [],
+                "booleans"     : { "YES" : true, "NO" : false },
+                "numbers"      : [ 0, 1, -2, 3.3, 4.4e5, 6.6e-7 ],
+                "strings"      : [ "This", [ "And" , "That", "And a \\"b" ] ],
+                "nothing"      : null
+            }
+        """
+        j = decoder.DECODER(test_json)
+        assert j == json.loads(test_json)
+
+        s = writer.dumps(j)
+        j2 = decoder.DECODER(s)
+        assert j2 == j
+
+        s2 = writer.dumps(j2)
+        assert s == s2
+
+        print('---')
+        print(s)
+        print('---')
+
+        assert s == EXPECTED
+
+
+EXPECTED = """\
+{'empty_object': {}, \
+'empty_array': [], \
+'booleans': {'YES': true, \
+'NO': false,}, \
+'numbers': [0, 1, -2, 3.3, 440000.0, 6.6e-07,], \
+'strings': ['This', ['And', 'That', 'And a "b',],], \
+'nothing': null,}
+"""
