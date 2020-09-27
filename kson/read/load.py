69560d44e08936bea717b66442dea50dd31dbf0e
Tom Ritchford <tom@swirly.com>
2020-09-27 14:58:02 +0200

    Add kson/read/load.py

---
+++ kson/read/load.py
@@ -0,0 +1,75 @@
+from . decoder import Decoder
+
+
+def decoder(
+    object_hook=None,
+    parse_float=None,
+    parse_int=None,
+    parse_constant=None,
+    object_pairs_hook=None
+):
+    decoder = Decoder()
+
+    if object_hook:
+        decoder.object = object_hook
+
+    if parse_float:
+        decoder.floating = parse_float
+
+    if parse_int:
+        decoder.integer = parse_int
+
+    if parse_constant:
+        decoder.nan = lambda: parse_constant('NaN')
+        decoder.inf = lambda: parse_constant('Infinity')
+        decoder.minus_inf = lambda: parse_constant('-Infinity')
+
+    if object_pairs_hook:
+        decoder.key_value = object_pairs_hook
+
+    return decoder
+
+
+def loads(
+    s,
+    *,
+    object_hook=None,
+    parse_float=None,
+    parse_int=None,
+    parse_constant=None,
+    object_pairs_hook=None
+):
+    """
+    Deserialize ``s`` (a ``str``, ``bytes`` or ``bytearray`` instance
+    containing a JSON document) to a Python object.
+
+    ``object_hook`` is an optional function that will be called with the
+    result of any object literal decode (a ``dict``). The return value of
+    ``object_hook`` will be used instead of the ``dict``. This feature
+    can be used to implement custom decoders (e.g. JSON-RPC class hinting).
+
+    ``object_pairs_hook`` is an optional function that will be called with the
+    result of any object literal decoded with an ordered list of pairs.  The
+    return value of ``object_pairs_hook`` will be used instead of the ``dict``.
+    This feature can be used to implement custom decoders.  If ``object_hook``
+    is also defined, the ``object_pairs_hook`` takes priority.
+
+    ``parse_float``, if specified, will be called with the string
+    of every JSON float to be decoded. By default this is equivalent to
+    float(num_str). This can be used to use another datatype or parser
+    for JSON floats (e.g. decimal.Decimal).
+
+    ``parse_int``, if specified, will be called with the string
+    of every JSON int to be decoded. By default this is equivalent to
+    int(num_str). This can be used to use another datatype or parser
+    for JSON integers (e.g. float).
+
+    ``parse_constant``, if specified, will be called with one of the
+    following strings: -Infinity, Infinity, NaN.
+    This can be used to raise an exception if invalid JSON numbers
+    are encountered.
+    """
+    d = decoder(
+        object_hook, parse_float, parse_int, parse_constant, object_pairs_hook
+    )
+    return d(s)
