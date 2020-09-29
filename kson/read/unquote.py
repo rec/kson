df5b7d876bc8bb3a5e563f29e53a060677fc21cf
Tom Ritchford <tom@swirly.com>
2020-09-29 14:19:14 +0200

    Refactor unquote.py for bytes

--- kson/read/unquote.py
+++ kson/read/unquote.py
@@ -1,70 +1,126 @@
+from dataclasses import dataclass, field
 from json import decoder, JSONDecodeError
+import copy
+import functools
 import re
-
-STRINGCHUNK = re.compile(
-    decoder.STRINGCHUNK.pattern.replace('"', "'"), decoder.FLAGS
-)
-BACKSLASH = dict(decoder.BACKSLASH)
-BACKSLASH["'"] = "'"
-del BACKSLASH['"']
 
 
 def unquote(s, strict=False):
-    quote = s[0]
-    if quote == '"':
-        _b = decoder.BACKSLASH
-        _m = decoder.STRINGCHUNK.match
-    else:
-        quote = "'"
-        _b = BACKSLASH
-        _m = STRINGCHUNK.match
+    use_bytes = not isinstance(s, str)
+    is_single_quote = s[0] in {"'", b"'"}
 
-    chunks = []
-    _append = chunks.append
-    begin = 0
-    end = 1
-    while 1:
-        chunk = _m(s, end)
-        if chunk is None:
-            raise JSONDecodeError("Unterminated string starting at", s, begin)
-        end = chunk.end()
-        content, terminator = chunk.groups()
-        # Content is contains zero or more unescaped string characters
+    unquoter = UNQUOTERS[is_single_quote][use_bytes]
+    return unquoter(s, strict)
+
+
+@dataclass
+class Unquoter:
+    quote: object = '"'
+    backslash: object = '\\'
+    string_chunk_re: object = decoder.STRINGCHUNK
+    backslash_dict: dict = field(default_factory=decoder.BACKSLASH.copy)
+    unicode_marker: object = 'u'
+
+    def to_single(self):
+        u = copy.deepcopy(self)
+        u.quote = "'"
+        pat = self.string_chunk_re.pattern.replace(self.quote, u.quote)
+        u.string_chunk_re = _compile(pat)
+        del u.backslash_dict[self.quote]
+        u.backslash_dict[u.quote] = u.quote
+        return u
+
+    def __call__(self, s, strict=False):
+        s = getattr(s, 'value', s)
+        chunks = []
+        end = 1
+
+        while end:
+            end = self._unquote_once(s, chunks, strict, end)
+        return self.quote[:0].join(chunks)
+
+    def _unquote_once(self, s, chunks, strict, end):
+        chunk = self.string_chunk_re.match(s, end)
+        if not chunk:
+            raise JSONDecodeError('Unterminated string', s)
+
+        (content, term), end = chunk.groups(), chunk.end()
+        # Content is zero or more unescaped string characters
         if content:
-            _append(content)
-        # Terminator is the end of string, a literal control character,
+            chunks.append(content)
+        # Term is the end of string, a literal control character,
         # or a backslash denoting that an escape sequence follows
-        if terminator == quote:
-            break
-        elif terminator != '\\':
+        if term == self.quote:
+            return
+        if term != self.backslash:
             if strict:
-                msg = "Invalid control character {0!r} at".format(terminator)
+                msg = "Invalid control character {0!r} at".format(term)
                 raise JSONDecodeError(msg, s, end)
-            else:
-                _append(terminator)
-                continue
+            chunks.append(term)
+            return end
         try:
             esc = s[end]
         except IndexError:
-            raise JSONDecodeError(
-                "Unterminated string starting at", s, begin
-            ) from None
+            raise JSONDecodeError('Unterminated string', s) from None
+
         # If not a unicode escape sequence, must be in the lookup table
-        if esc != 'u':
+        if esc != self.unicode_marker:
             try:
-                char = _b[esc]
+                char = self.backslash_dict[esc]
             except KeyError:
                 msg = "Invalid \\escape: {0!r}".format(esc)
-                raise JSONDecodeError(msg, s, end)
-            end += 1
-        else:
-            uni = decoder._decode_uXXXX(s, end)
-            end += 5
-            if 0xD800 <= uni <= 0xDBFF and s[end : end + 2] == '\\u':
-                uni2 = decoder._decode_uXXXX(s, end + 1)
-                if 0xDC00 <= uni2 <= 0xDFFF:
-                    uni = 0x10000 + (((uni - 0xD800) << 10) | (uni2 - 0xDC00))
-                    end += 6
+                raise JSONDecodeError(msg, s, end) from None
+            else:
+                chunks.append(char)
+                return end + 1
+
+        uni = _decode_uXXXX(s, end)
+        end += 5
+        ucode = self.backslash + self.unicode_marker
+        if 0xD800 <= uni <= 0xDBFF and s[end : end + 2] == ucode:
+            uni2 = _decode_uXXXX(s, end + 1)
+            if 0xDC00 <= uni2 <= 0xDFFF:
+                uni = 0x10000 + (((uni - 0xD800) << 10) | (uni2 - 0xDC00))
+                end += 6
+        if isinstance(s, str):
             char = chr(uni)
-        _append(char)
-    return ''.join(chunks)
+        else:
+            char = bytes([uni])
+        chunks.append(char)
+        return end
+
+
+def to_bytes(x):
+    if isinstance(x, str):
+        return x.encode()
+    if isinstance(x, list):
+        return [to_bytes(i) for i in x]
+    if isinstance(x, dict):
+        return {to_bytes(k): to_bytes(v) for k, v in x.items()}
+    if isinstance(x, re.Pattern):
+        return _compile(to_bytes(x.pattern))
+
+    df = getattr(x, '__dataclass_fields__', None)
+    if df is not None:
+        fields = {k: to_bytes(getattr(x, k)) for k in df}
+        return x.__class__(**fields)
+
+    return x
+
+
+def _decode_uXXXX(s, pos):
+    esc = s[pos + 1 : pos + 5]
+    if len(esc) == 4 and esc[1] not in {'x', 'X', b'x', b'X'}:
+        try:
+            return int(esc, 16)
+        except ValueError:
+            pass
+    raise JSONDecodeError('Invalid \\uXXXX escape', s, pos)
+
+
+_compile = functools.partial(re.compile, flags=decoder.FLAGS)
+
+UNQUOTERS = (
+    (Unquoter(), to_bytes(Unquoter())),
+    (Unquoter().to_single(), to_bytes(Unquoter().to_single())),
+)
