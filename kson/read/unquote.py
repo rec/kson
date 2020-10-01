6ef550b9d4a5c20db685b641467c3b8bcf28e0d4
Tom Ritchford <tom@swirly.com>
2020-10-01 11:16:36 +0200

    Refactor and simplify unquote.py

--- kson/read/unquote.py
+++ kson/read/unquote.py
@@ -1,76 +1,82 @@
-from . import quotes
 from json import JSONDecodeError
 
 
 def unquote(s, strict=False):
-    s = getattr(s, "value", s)
+    from . import quotes
+
+    s = getattr(s, 'value', s)
     q = quotes.quotes(s)
     chunks = []
     end = 1
 
     while end:
         end = _unquote_once(q, s, chunks, strict, end)
+
     return q.quote[:0].join(chunks)
 
 
 def _unquote_once(quotes, s, chunks, strict, end):
+    def error(msg, *items):
+        ss = s if isinstance(s, str) else s.decode()
+        return JSONDecodeError(msg.format(*items), ss, end)
+
+    def decode_uXXXX(pos):
+        esc = s[pos + 1 : pos + 5]
+        if len(esc) == 4 and esc[1] not in {'x', 'X', b'x', b'X'}:
+            try:
+                return int(esc, 16)
+            except ValueError:
+                pass
+        raise error('Invalid \\uXXXX escape')
+
     chunk = quotes.string_chunk_re.match(s, end)
     if not chunk:
-        raise JSONDecodeError("Unterminated string", str(s), end)
+        raise error('Unterminated string')
 
-    (content, term), end = chunk.groups(), chunk.end()
-    # Content is zero or more unescaped string characters
-    if content:
-        chunks.append(content)
-    # Term is the end of string, a literal control character,
-    # or a backslash denoting that an escape sequence follows
+    # `content` is zero or more unescaped string characters.
+    #
+    # `term` is the end of string, a literal control character,
+    # or a backslash denoting that an escape sequence follows.
+    #
+    content, term = chunk.groups()
+    chunks.append(content)
     if term == quotes.quote:
         return
+
+    end = chunk.end()
+
     if term != quotes.backslash:
         if strict:
-            msg = "Invalid control character {0!r} at".format(term)
-            raise JSONDecodeError(msg, s, end)
+            raise error('Invalid control character {0!r} at', term)
+
         chunks.append(term)
         return end
+
     try:
         esc = s[end : end + 1]
     except IndexError:
-        raise JSONDecodeError("Unterminated string", str(s), end) from None
+        raise error('Unterminated string') from None
 
-    # If not a unicode escape sequence, must be in the lookup table
+    # If it's not a unicode escape sequence, it should be in the lookup table
     if esc != quotes.unicode_marker:
         try:
             char = quotes.backslash_dict[esc]
         except KeyError:
-            msg = "Invalid \\escape: {0!r}".format(esc)
-            if not isinstance(s, str):
-                s = s.decode()
-            raise JSONDecodeError(msg, s, end) from None
-        else:
-            chunks.append(char)
-            return end + 1
+            raise error('Invalid \\escape: {0!r}', esc) from None
 
-    uni = _decode_uXXXX(s, end)
+        chunks.append(char)
+        return end + 1
+
+    uni = decode_uXXXX(end)
     end += 5
     ucode = quotes.backslash + quotes.unicode_marker
+
     if 0xD800 <= uni <= 0xDBFF and s[end : end + 2] == ucode:
-        uni2 = _decode_uXXXX(s, end + 1)
+        uni2 = decode_uXXXX(end + 1)
         if 0xDC00 <= uni2 <= 0xDFFF:
             uni = 0x10000 + (((uni - 0xD800) << 10) | (uni2 - 0xDC00))
             end += 6
-    if isinstance(s, str):
-        char = chr(uni)
-    else:
-        char = bytes([uni])
+    char = chr(uni) if isinstance(s, str) else bytes([uni])
     chunks.append(char)
+
     return end
-
-
-def _decode_uXXXX(s, pos):
-    esc = s[pos + 1 : pos + 5]
-    if len(esc) == 4 and esc[1] not in {"x", "X", b"x", b"X"}:
-        try:
-            return int(esc, 16)
-        except ValueError:
-            pass
-    raise JSONDecodeError("Invalid \\uXXXX escape", s, pos)
